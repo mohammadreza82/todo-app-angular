2c8a9f0ba072bf0d6367f9d0780d85d3a1c958cd
MohammadReza-Jamshidi <Jamshidi.m.1382@gmail.com>
2024-11-10 22:13:02 +0330

    data-bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-root',
   // standalone = ماژول و کامپوننت رو یکی میکنه با هم دیگه
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HeaderComponent],
+  imports: [CommonModule, FormsModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'todo-app-angular';
+  // stringBinding
+  user: string = 'MohammadReza Jamshidi'
+  id = 'n-1'
+  handleClick = () => {
+    alert(`Hello ${this.user}`)
+  }
+  hansleMouseEnter = (e: MouseEvent) => {
+    console.log({ x: e.x, y: e.y })
+  }
 }
